4a4af4883485539ab840e56184d4f9527bf4fdf6
mdupree <mikerdupree@gmail.com>
2023-02-06 07:50:38 -0800

    WIP recursive listing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use glob::glob;
-use glob::Paths;
 use std::env;
 use std::os::linux::fs::MetadataExt;
 use std::process::Command;
@@ -24,24 +23,15 @@ fn main() {
         println!("\x1b[{}m {}\x1b[0m", 99, 99);
     }
 
-    println!("{} \x1b[96m ({})\x1b[0m", env::current_dir().unwrap().display(), git_branch());
     let opts = get_args();
+    let filepath_glob = format!("{}{}", opts.filepath.as_str(), "*");
 
-    let file_path_glob = format!("{}{}", opts.filepath.as_str(), "*");
-    let files = glob(file_path_glob.as_str()).expect("Failed to read glob pattern");
-
-    if opts.options == String::from("") {
-        // TODO make a grid output
-        for entry in glob(file_path_glob.as_str()).expect("Failed to read glob pattern") {
-            match entry {
-                Ok(path) => print_file(path),
-                Err(e) => println!("{:?}", e),
-            }
-        }
-    } else {
-        print_files_detailed(glob(file_path_glob.as_str()).expect("Failed to read glob pattern"));
-    }
-    println!("Total: {}", files.count());
+    println!(
+        "{} \x1b[96m ({})\x1b[0m",
+        env::current_dir().unwrap().display(),
+        git_branch()
+    );
+    print_files(filepath_glob, opts);
 }
 
 #[derive(Debug)]
@@ -75,7 +65,12 @@ fn git_branch() -> String {
         .output()
         .expect("failed to execute process");
 
-    format!("{}", String::from_utf8_lossy(&output.stdout).replace("* ", "").replace("\n", ""))
+    format!(
+        "{}",
+        String::from_utf8_lossy(&output.stdout)
+            .replace("* ", "")
+            .replace("\n", "")
+    )
 }
 
 fn format_git(path: &PathBuf) -> String {
@@ -102,6 +97,7 @@ fn format_git(path: &PathBuf) -> String {
     }
     format!("{}", "")
 }
+
 fn format_file_git_status(path: &PathBuf) -> String {
     let output = Command::new("/usr/bin/git")
         .arg("status")
@@ -137,7 +133,7 @@ fn print_file(path: PathBuf) {
 
 fn format_file(path: &PathBuf) -> String {
     if path.is_dir() {
-        return format!("\x1b[{}m  \x1b[0m {}", 93, path.display(),);
+        return format!("\x1b[{}m  \x1b[0m{}", 93, path.display(),);
     }
 
     let mut file_icon = "";
@@ -152,7 +148,7 @@ fn format_file(path: &PathBuf) -> String {
             _ => (),
         }
     }
-    return format!("\x1b[{}m {} \x1b[0m {}", 92, file_icon, path.display());
+    return format!("\x1b[{}m {} \x1b[0m{}", 92, file_icon, path.display());
 }
 
 fn format_permissions(path: &PathBuf) -> String {
@@ -182,7 +178,82 @@ fn dir_size(path: impl Into<PathBuf>) -> io::Result<u64> {
     dir_size(fs::read_dir(path.into())?)
 }
 
-fn print_files_detailed(files: Paths) {
+fn print_files(filepath_glob: String, opts: CommandOptions) {
+    println!("selected Opts: {}", opts.options);
+    if opts.options == String::from("") {
+        print_files_simple(filepath_glob);
+        return;
+    }
+
+    if opts.options == String::from("r") {
+        println!("Running Recursive Listing");
+        print_files_recursive(filepath_glob);
+        return;
+    }
+
+    print_files_detailed(filepath_glob);
+}
+
+fn print_files_simple(filepath_glob: String) {
+    let files = glob(filepath_glob.as_str()).expect("Failed to read glob pattern");
+    let mut file_count = 0;
+    let mut list_output = String::new();
+
+    for entry in files {
+        match entry {
+            Ok(path) => {
+                file_count += 1;
+                list_output.push_str(format!("{}  ", format_file(&path)).as_str())
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
+
+    println!("{}\n {} files", list_output, file_count);
+}
+
+fn format_files_recursive(filepath_glob: &String, level: &u16, max_depth: &u16) -> String {
+    if level >= max_depth {
+        return String::new();
+    }
+
+    let files = glob(filepath_glob.as_str()).expect("Failed to read glob pattern");
+    let mut file_count = 0;
+    let mut list_output = String::new();
+
+    for entry in files {
+        match entry {
+            Ok(path) => {
+                file_count += 1;
+                list_output.push_str(format!("- {}\n", format_file(&path)).as_str());
+                if !path.is_dir() {
+                    continue;
+                }
+
+                let next_level = level + 1;
+                list_output.push_str(
+                    format_files_recursive(format!("{}", &next_level, max_depth).as_str(),
+                );
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
+
+    format!("{} {}\n", level, list_output)
+}
+
+fn print_files_recursive(filepath_glob: String) {
+    let current_level = 0;
+    let max_depth = 3;
+    println!(
+        "{}",
+        format_files_recursive(&filepath_glob, &current_level, &max_depth)
+    );
+}
+
+fn print_files_detailed(filepath_glob: String) {
+    let files = glob(filepath_glob.as_str()).expect("Failed to read glob pattern");
+    let mut file_count = 0;
     let mut table = Table::new();
     //table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
     let format = format::FormatBuilder::new()
@@ -194,6 +265,7 @@ fn print_files_detailed(files: Paths) {
         )
         .padding(1, 1)
         .build();
+
     table.set_format(format);
 
     table.set_titles(row![
@@ -209,6 +281,7 @@ fn print_files_detailed(files: Paths) {
     for file in files {
         match file {
             Ok(path) => {
+                file_count += 1;
                 table.add_row(row![
                     format_file(&path),
                     format_git(&path),
@@ -229,4 +302,5 @@ fn print_files_detailed(files: Paths) {
     }
 
     table.printstd();
+    println!("Total: {}", file_count);
 }
